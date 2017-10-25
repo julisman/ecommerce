a4a4b07cc6f43d99ea114e16bbdf9c8869bc9f54
agungjulisman <agung@tabsquare.com>
2017-10-26 05:56:38 +0700

    test

--- app/route/routes.js
+++ app/route/routes.js
@@ -2,7 +2,7 @@
 module.exports = function(app) {
     app.get('/', (req, res) => {
         res.send({
-            status: 'Api is running!'
+            status: 'Api is running!!!'
         })
     })
 }
